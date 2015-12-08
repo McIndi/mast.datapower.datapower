2642765c923c02dd495d7be4d76e447ba165ba80
ilovetux <me@ilovetux.com>
2015-12-08 17:41:39 -0500

    ENHANCEMENT: Fix formatting

--- setup.py
+++ setup.py
@@ -4,14 +4,16 @@ from setuptools import setup
 
 setup(name='mast.datapower.datapower',
       version='2.1.0',
-      description='A library to interact with IBM DataPower appliances XML Management Interface and CLI Management Interface',
+      description='A library to interact with IBM DataPower appliances '
+                  'XML Management Interface and CLI Management Interface',
       author='Clifford Bressette IV',
       author_email='cliffordbressette@mcindi.com',
       maintainer="Clifford Bressette",
       url='https://github.com/mcindi/mast.datapower.datapower',
       namespace_packages=["mast", "mast.datapower"],
-      packages=['mast', 'mast.datapower', 'mast.datapower.datapower', 'mast.datapower.datapower.et'],
+      packages=['mast',
+                'mast.datapower',
+                'mast.datapower.datapower',
+                'mast.datapower.datapower.et'],
       license="GPL v2",
-      # TODO: Get a list of requirements (Most of this needs only std library, but some dependencies exist)
-      install_requires=[]
-     )
+      install_requires=[])
