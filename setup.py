4dd3e8a39d6d131de61936090542a0749de02ea6
ilovetux <me@ilovetux.com>
2016-09-09 13:51:39 -0400

    Bump version

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from setuptools import setup
 
 setup(name='mast.datapower.datapower',
-      version='2.1.0',
+      version='2.2.0',
       description='A library to interact with IBM DataPower appliances '
                   'XML Management Interface and CLI Management Interface',
       author='Clifford Bressette IV',
